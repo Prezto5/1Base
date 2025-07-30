5acd3a8de024a1aada6edf6fc09f513436d5beff
Prezto5 <passionteam35@gmail.com>
2025-07-30 15:37:13 +0300

    Добавлен Astro проект my-app и обновлены MDC файлы

--- frontend/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function HomePage() {
-  // Статический редирект на базовый продукт и регион
-  // Избегаем блокирующего API запроса при каждом посещении главной страницы
-  redirect('/baza-horeca/russia');
-}
